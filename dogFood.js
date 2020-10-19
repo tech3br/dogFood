ad7cf8014633c576ac3473bf1040f1c14699f57b
tech3br <dfsdireito95@gmail.com>
2020-10-18 22:20:49 -0300

    Fixing text comment in DogFood.js

--- dogFood.js
+++ dogFood.js
@@ -3,6 +3,7 @@ const age = 1;
 const weight = 8;
 const t = 0.2;
 
-// Defininido Função Reutilizável
+// Definindo Função Reutilizável
 const dogFood = (age, weight, t) => `${age * weight * t} pounds of dog food`;
 console.log(dogFood(age, weight, t));
+
